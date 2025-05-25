beed1de803d8b6cd4084b8ddf4b8937a39be8ae3
Etativel <farhanmaulana4611@gmail.com>
2025-05-25 18:42:14 +0800

    update img dir

--- src/assets/projectsImage/index.js
+++ src/assets/projectsImage/index.js
@@ -1,24 +1,24 @@
-import foodlens1 from "../images/foodlens_img/foodlens_1.jpg";
-import foodlens2 from "../images/foodlens_img/foodlens_2.jpg";
-import foodlens3 from "../images/foodlens_img/foodlens_3.jpg";
-import foodlens4 from "../images/foodlens_img/foodlens_4.jpg";
-import foodlens5 from "../images/foodlens_img/foodlens_5.jpg";
+import foodlens1 from "./foodlens_img/foodlens_1.jpg";
+import foodlens2 from "./foodlens_img/foodlens_2.jpg";
+import foodlens3 from "./foodlens_img/foodlens_3.jpg";
+import foodlens4 from "./foodlens_img/foodlens_4.jpg";
+import foodlens5 from "./foodlens_img/foodlens_5.jpg";
 
-import node1_l from "../images/nodesblog_img/light/1.jpg";
-import node2_l from "../images/nodesblog_img/light/2.jpg";
-import node3_l from "../images/nodesblog_img/light/3.jpg";
-import node4_l from "../images/nodesblog_img/light/4.jpg";
-import node5_l from "../images/nodesblog_img/light/5.jpg";
+import node1_l from "./nodesblog_img/light/1.jpg";
+import node2_l from "./nodesblog_img/light/2.jpg";
+import node3_l from "./nodesblog_img/light/3.jpg";
+import node4_l from "./nodesblog_img/light/4.jpg";
+import node5_l from "./nodesblog_img/light/5.jpg";
 
-import node1_b from "../images/nodesblog_img/dark/1.jpg";
-import node2_b from "../images/nodesblog_img/dark/2.jpg";
-import node3_b from "../images/nodesblog_img/dark/3.jpg";
-import node4_b from "../images/nodesblog_img/dark/4.jpg";
-import node5_b from "../images/nodesblog_img/dark/5.jpg";
+import node1_b from "./nodesblog_img/dark/1.jpg";
+import node2_b from "./nodesblog_img/dark/2.jpg";
+import node3_b from "./nodesblog_img/dark/3.jpg";
+import node4_b from "./nodesblog_img/dark/4.jpg";
+import node5_b from "./nodesblog_img/dark/5.jpg";
 
-import cv1 from "../images/CvMaker_img/1.jpg";
-import cv2 from "../images/CvMaker_img/2.jpg";
-import cv3 from "../images/CvMaker_img/3.jpg";
+import cv1 from "./CvMaker_img/1.jpg";
+import cv2 from "./CvMaker_img/2.jpg";
+import cv3 from "./CvMaker_img/3.jpg";
 
 export {
   cv1,
