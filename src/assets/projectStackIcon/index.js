1deffce35c08d45c5db92226d42d9701da9d70cc
Etativel <farhanmaulana4611@gmail.com>
2025-05-25 16:09:43 +0800

    index project stack icon

---
+++ src/assets/projectStackIcon/index.js
@@ -0,0 +1,66 @@
+import expressIconBlack from "../favicons/express-original.svg";
+import expressIconWhite from "../favicons/express-white.svg";
+
+import nodeIcon from "../favicons/nodejs-original.svg";
+
+import postgresIconBlack from "../favicons/postgresql-original-wordmark.svg";
+import postgresIconWhite from "../favicons/postgresql-white.svg";
+
+import prisma from "../favicons/prisma-blue.svg";
+
+import passport from "../favicons/passport.svg";
+
+import cloudinary from "../favicons/cloudinary-svgrepo-com.svg";
+
+import reactRouterWhite from "../favicons/react-router-white.svg";
+import reactRouterBlack from "../favicons/react-router.svg";
+
+import vite from "../favicons/vite.svg";
+
+import tailwind from "../favicons/tailwindcss-plain.svg";
+
+import materialui from "../favicons/materialui-original.svg";
+import chartjs from "../favicons/chartjs.svg";
+
+import javascript from "../favicons/javascript-original.svg";
+import css from "../favicons/css3-original-wordmark.svg";
+
+import railwayDark from "../favicons/railway-dark.svg";
+import railwayLight from "../favicons/railway.svg";
+
+import flaskDark from "../favicons/flask-dark.svg";
+import flaskLight from "../favicons/flask-original.svg";
+
+import pyTorch from "../favicons/pytorch-svgrepo-com.svg";
+
+import openAiLight from "../favicons/openai-svgrepo-com.svg";
+import openAiDark from "../favicons/open-ai-white.svg";
+
+import huggingFace from "../favicons/hugging-face-svgrepo-com.svg";
+
+export {
+  huggingFace,
+  pyTorch,
+  flaskLight,
+  flaskDark,
+  javascript,
+  css,
+  expressIconBlack,
+  expressIconWhite,
+  postgresIconBlack,
+  nodeIcon,
+  prisma,
+  tailwind,
+  vite,
+  chartjs,
+  materialui,
+  reactRouterBlack,
+  reactRouterWhite,
+  postgresIconWhite,
+  passport,
+  cloudinary,
+  railwayDark,
+  railwayLight,
+  openAiLight,
+  openAiDark
+};
---
+++ src/assets/projectsImage/index.js
@@ -0,0 +1,35 @@
+import foodlens1 from "./foodlens_img/foodlens_1.jpg";
+import foodlens2 from "./foodlens_img/foodlens_2.jpg";
+import foodlens3 from "./foodlens_img/foodlens_3.jpg";
+import foodlens4 from "./foodlens_img/foodlens_4.jpg";
+import foodlens5 from "./foodlens_img/foodlens_5.jpg";
+
+import node1_l from "./nodesblog_img/light/1.jpg";
+import node2_l from "./nodesblog_img/light/2.jpg";
+import node3_l from "./nodesblog_img/light/3.jpg";
+import node4_l from "./nodesblog_img/light/4.jpg";
+import node5_l from "./nodesblog_img/light/5.jpg";
+
+import node1_b from "./nodesblog_img/dark/1.jpg";
+import node2_b from "./nodesblog_img/dark/2.jpg";
+import node3_b from "./nodesblog_img/dark/3.jpg";
+import node4_b from "./nodesblog_img/dark/4.jpg";
+import node5_b from "./nodesblog_img/dark/5.jpg";
+
+export {
+  foodlens1,
+  foodlens2,
+  foodlens3,
+  foodlens4,
+  foodlens5,
+  node1_b,
+  node2_b,
+  node3_b,
+  node4_b,
+  node5_b,
+  node1_l,
+  node2_l,
+  node3_l,
+  node4_l,
+  node5_l
+};
